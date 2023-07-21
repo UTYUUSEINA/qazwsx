4b3b4ba85d15d5e1fec7afd9633c17357157724f
seina <newskoihosi@yahoo.co.jp>
2023-07-21 14:58:40 +0900

    tooy

---
+++ src/Qpal.java
@@ -0,0 +1,12 @@
+
+public class Qpal {
+
+	public static void main(String[] args) {
+		System.out.println("qawsed");
+		System.out.println("qawsed");
+		System.out.println("qawsed");
+		
+		
+	}
+
+}
